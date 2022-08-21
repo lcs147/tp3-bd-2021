38a3c4130ccb7eba7c94fdc301023414d08bc0ec
Lucas Castro <lcs147@hotmail.com>
2022-08-21 16:24:20 -0400

    update 5.sql sqlite

--- sqlite-queries/5.sql
+++ sqlite-queries/5.sql
@@ -2,27 +2,26 @@
 
 
 select
-	n_name,
-	sum(l_extendedprice * (1 - l_discount)) as revenue
+  n_name,
+  sum((l_extendedprice * (1 - l_discount))) as revenue
 from
-	customer,
-	orders,
-	lineitem,
-	supplier,
-	nation,
-	region
-where
-	c_custkey = o_custkey
-	and l_orderkey = o_orderkey
-	and l_suppkey = s_suppkey
-	and c_nationkey = s_nationkey
-	and s_nationkey = n_nationkey
-	and n_regionkey = r_regionkey
-	and r_name = 'MIDDLE EAST'
-	and o_orderdate >= date '1997-01-01'
-	and o_orderdate < date '1997-01-01' + interval '1' year
-group by
-	n_name
-order by
-	revenue desc
-LIMIT 1;
+  customer,
+  orders,
+  lineitem,
+  supplier,
+  nation,
+  region
+where (
+  c_custkey = o_custkey
+  and l_orderkey = o_orderkey
+  and l_suppkey = s_suppkey
+  and c_nationkey = s_nationkey
+  and s_nationkey = n_nationkey
+  and n_regionkey = r_regionkey
+  and r_name = 'MIDDLE EAST'
+  and o_orderdate >= '1997-01-01'
+  and o_orderdate < strftime('%Y-%m-%d %H:%M:%f', '1997-01-01', (cast(1.2E1 as varchar) || ' months'))
+)
+group by n_name
+order by revenue desc
+limit 1;
